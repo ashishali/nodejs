5bc6d9cbaf089b780d919cfeed569b31b5302160
ashishali <ashishbearyy@gmail.com>
2021-03-05 20:34:21 +0530

    new way

--- heavyops(disk/network)/disk.js
+++ heavyops(disk/network)/disk.js
@@ -1,18 +1,5 @@
-// var access = require('/Users/ashishybeary/Documents/nodejs/heavyops(disk/network)/data.json')
-// console.log("Way 1:")
-// console.log(access.kelsa)
-// console.log('')
-// console.log('')
-// console.log("way 2:")
-// var access2 = require('fs')
-// access2.readFile('/Users/ashishybeary/Documents/nodejs/heavyops(disk/network)/data.json', 'utf-8', (err, data) => {
-//     x = JSON.parse(data)
-//     console.log(x.name)
-//     console.log(x.kelsa)
-// })
-
-
 var access = require('fs')
+const path = require('path')
 access.readFile('/Users/ashishybeary/Documents/nodejs/heavyops(disk/network)/data.json', 'utf-8', (err,data) => {
     var parser = JSON.parse(data)
     console.log("Way 2:")
@@ -28,3 +15,17 @@ console.log(access.name)
 console.log(access.kelsa)
 console.log('')
 console.log('')
+
+console.log("Directory:", __dirname)
+console.log("Filename:", __filename)
+
+console.log('')
+console.log('')
+console.log('Fetching with path module...')
+console.log(`Filename: ${path.basename(__filename)}`)
+console.log(`Directory: ${path.basename(__dirname)}`)
+console.log('')
+console.log('')
+
+
+console.log(process.argv)
